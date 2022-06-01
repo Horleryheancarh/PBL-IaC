5c62974b1fddf36b78fc118956cb262ebf0583ec
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-06-01 15:57:04 +0100

    Added public route table, route and associations

--- routes_tables.tf
+++ routes_tables.tf
@@ -17,3 +17,28 @@ resource "aws_route_table_association" "private_subnet_association" {
   route_table_id = aws_route_table.private_rtb.id
 }
 
+# Create public route table
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-public-rtb", var.name)
+    }
+  )
+}
+
+# Create route for public route table and attach the internet gateway
+resource "aws_route" "public_rtb_route" {
+  route_table_id         = aws_route_table.public_rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate public subnets to public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(aws_subnet.public_subnet[*].id)
+  subnet_id      = element(aws_subnet.public_subnet[*].id, count.index)
+  route_table_id = aws_route_table.public_rtb.id
+}
