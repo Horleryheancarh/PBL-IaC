9130cb9341932d0b78dfd57fbbf136cd01cfc6c5
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-06-02 13:42:25 +0100

    added create efs, efs mount target and efs access points

--- efs.tf
+++ efs.tf
@@ -24,3 +24,68 @@ resource "aws_kms_alias" "alias" {
   target_key_id = aws_kms_key.yinkadevops_kms.key_id
 }
 
+# Create EFS
+resource "aws_efs_file_system" "yinkadevops_efs" {
+  encrypted  = true
+  kms_key_id = aws_kms_key.yinkadevops_kms.arn
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "yinkadevops-efs"
+    }
+  )
+}
+
+# Create mount targets for EFS
+resource "aws_efs_mount_target" "subnet_1" {
+  file_system_id  = aws_efs_file_system.yinkadevops_efs.id
+  subnet_id       = aws_subnet.private_subnet[2].id
+  security_groups = [aws_security_group.datalayer_sg.id]
+}
+
+resource "aws_efs_mount_target" "subnet_2" {
+  file_system_id  = aws_efs_file_system.yinkadevops_efs.id
+  subnet_id       = aws_subnet.private_subnet[3].id
+  security_groups = [aws_security_group.datalayer_sg.id]
+}
+
+# Create access point for wordpress
+resource "aws_efs_access_point" "wordpress_ap" {
+  file_system_id = aws_efs_file_system.yinkadevops_efs.id
+
+  posix_user {
+    gid = 0
+    uid = 0
+  }
+
+  root_directory {
+    path = "/wordpress"
+
+    creation_info {
+      owner_gid   = 0
+      owner_uid   = 0
+      permissions = 0755
+    }
+  }
+}
+
+# Create access point for tooling
+resource "aws_efs_access_point" "tooling_ap" {
+  file_system_id = aws_efs_file_system.yinkadevops_efs.id
+
+  posix_user {
+    gid = 0
+    uid = 0
+  }
+
+  root_directory {
+    path = "/tooling"
+
+    creation_info {
+      owner_gid   = 0
+      owner_uid   = 0
+      permissions = 0755
+    }
+  }
+}
