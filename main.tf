dbaab2ce6f4bf142db172a418cfa6a94e31b6803
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-06-15 13:56:47 +0100

    s3

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-# Note: The bucket name may not work for you since buckets are unique globally in AWS, so you must give it a unique name.
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "yheancarh-dev-terraform-bucket"
-}
+# # Note: The bucket name may not work for you since buckets are unique globally in AWS, so you must give it a unique name.
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "yheancarh-dev-terraform-bucket"
+# }
 
-# Enable versioning so we can see the full revision history of our state files
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+# # Enable versioning so we can see the full revision history of our state files
+# resource "aws_s3_bucket_versioning" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-# Enable server-side encryption by default
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# # Enable server-side encryption by default
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state.bucket
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
 
 resource "aws_dynamodb_table" "terraform_locks" {
